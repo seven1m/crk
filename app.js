9c338ebbcea7a1dc471d8c7c6a7023d25d145563
Tim Morgan <tim@timmorgan.org>
2011-04-27 23:33:57 -0500

    Turn on logging in Express.

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
+app.configure('development', function(){
+  app.use(express.logger());
+});
+
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
