b4265e9babce652db15142de5af94f6bb7ce3f19
Tim Morgan <tim@timmorgan.org>
2011-04-28 00:31:45 -0500

    Selectable notes.
    * single-click on a note to get its unique url
    * a red outline will appear
    * click off the note to de-select

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -25,7 +25,7 @@ App.NoteView = Backbone.View.extend({
   className: 'note',
 
   initialize: function() {
-    _.bindAll(this, 'render', 'editStart', 'editStop', 'dragStart', 'dragStop');
+    _.bindAll(this, 'render', 'select', 'editStart', 'editStop', 'dragStart', 'dragStop');
     this.model.bind('change', this.render);
   },
 
@@ -39,16 +39,25 @@ App.NoteView = Backbone.View.extend({
     ).draggable({
       start: this.dragStart,
       stop:  this.dragStop
-    }).dblclick(
+    }).click(
+      this.select
+    ).dblclick(
       this.editStart
     ).find('.content').text(
       this.model.get('content')
     );
+    if(this.model.get('selected')) $(this.el).addClass('note-selected');
+    else $(this.el).removeClass('note-selected');
     return this;
   },
 
   buildPin: function() {
     return $('<div/>', {'class': 'pin ' + this.model.get('pin') + '-pin'}).click(this.model.changePin);
+  },
+
+  select: function(event) {
+    location.hash = 'notes/' + this.model.id;
+    event.stopPropagation();
   },
 
   editStart: function(event) {
@@ -84,29 +93,50 @@ App.NoteView = Backbone.View.extend({
 App.NotesCollection = Backbone.Collection.extend({
   model: App.Note,
 
-  url: '/notes'
+  url: '/notes',
+
+  clearSelection: function() {
+    this.each(function(note) {
+      note.set({'selected': false});
+    });
+  }
+
 });
 
 App.BoardController = Backbone.Controller.extend({
   routes: {
-    // nothing yet
+    '':          'index',
+    'notes/:id': 'show'
   },
 
   initialize: function() {
+    _.bindAll(this, 'show');
     $('#board').empty()
-    var notes = new App.NotesCollection;
-    notes.fetch({success: function(collection){
+    this.notes = new App.NotesCollection();
+    this.notes.fetch({success: function(collection){
       collection.each(function(note) {
         var view = new App.NoteView({model: note});
         $('#board').append(view.render().el);
       });
+      Backbone.history.start();
     }});
+    $('#board').click(function() {
+      location.hash = '';
+    });
+  },
+
+  index: function() {
+    this.notes.clearSelection();
+  },
+
+  show: function(id) {
+    this.notes.clearSelection();
+    var note = this.notes.get(id)
+    if(note) note.set({'selected': true});
   }
 
 });
 
 $(function() {
-  var controller = new App.BoardController;
-  Backbone.history = new Backbone.History
-  Backbone.history.start();
+  var controller = new App.BoardController();
 });
