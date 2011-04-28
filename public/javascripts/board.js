1fc952ca5504b2bc5295ab5afd9c84ed881b30ec
Tim Morgan <tim@timmorgan.org>
2011-04-27 23:13:21 -0500

    Reformat js a bit. No actual changes.

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,81 +1,78 @@
-var App = {
+var App = {};
 
-  Note: Backbone.Model.extend({
-    pinColors: ['red', 'green', 'blue', 'yellow'],
+App.Note = Backbone.Model.extend({
+  pinColors: ['red', 'green', 'blue', 'yellow'],
 
-    initialize: function() {
-      _.bindAll(this, 'changePin');
-    },
+  initialize: function() {
+    _.bindAll(this, 'changePin');
+  },
 
-    changePin: function() {
-      var color = _.indexOf(this.pinColors, this.get('pin')) + 1;
-      if(color > this.pinColors.length-1) color = 0;
-      this.set({'pin': this.pinColors[color]});
-    },
+  changePin: function() {
+    var color = _.indexOf(this.pinColors, this.get('pin')) + 1;
+    if(color > this.pinColors.length-1) color = 0;
+    this.set({'pin': this.pinColors[color]});
+  },
 
-  }),
+});
 
-  NoteView: Backbone.View.extend({
-    className: 'note',
+App.NoteView = Backbone.View.extend({
+  className: 'note',
 
-    initialize: function() {
-      _.bindAll(this, 'render', 'editStart', 'editStop', 'dragStart', 'dragStop');
-      this.model.bind('change', this.render);
-    },
+  initialize: function() {
+    _.bindAll(this, 'render', 'editStart', 'editStop', 'dragStart', 'dragStop');
+    this.model.bind('change', this.render);
+  },
 
-    render: function() {
-      console.log('render');
-      $(this.el).css({
-        left: this.model.get('left') + 'px',
-        top:  this.model.get('top') + 'px'
-      }).html(
-        this.buildPin().add(
-        $('<div/>', {'class': 'content'}))
-      ).draggable({
-        start: this.dragStart,
-        stop:  this.dragStop
-      }).dblclick(
-        this.editStart
-      ).find('.content').text(
-        this.model.get('content')
-      );
-      return this;
-    },
+  render: function() {
+    $(this.el).css({
+      left: this.model.get('left') + 'px',
+      top:  this.model.get('top') + 'px'
+    }).html(
+      this.buildPin().add(
+      $('<div/>', {'class': 'content'}))
+    ).draggable({
+      start: this.dragStart,
+      stop:  this.dragStop
+    }).dblclick(
+      this.editStart
+    ).find('.content').text(
+      this.model.get('content')
+    );
+    return this;
+  },
 
-    buildPin: function() {
-      return $('<div/>', {'class': 'pin ' + this.model.get('pin') + '-pin'}).click(this.model.changePin);
-    },
+  buildPin: function() {
+    return $('<div/>', {'class': 'pin ' + this.model.get('pin') + '-pin'}).click(this.model.changePin);
+  },
 
-    editStart: function(event) {
-      $('<textarea/>').appendTo(
-        this.$('.content').empty()
-      ).blur(
-        this.editStop
-      ).val(
-        this.model.get('content')
-      )[0].focus();
-    },
+  editStart: function(event) {
+    $('<textarea/>').appendTo(
+      this.$('.content').empty()
+    ).blur(
+      this.editStop
+    ).val(
+      this.model.get('content')
+    )[0].focus();
+  },
 
-    editStop: function(event) {
-      var content = this.$('textarea').val();
-      this.model.set({content: content}); // fires change, then render()
-    },
+  editStop: function(event) {
+    var content = this.$('textarea').val();
+    this.model.set({content: content}); // fires change, then render()
+  },
 
-    dragStart: function(event, ui) {
-      $(this.el).addClass('note-drag');
-    },
+  dragStart: function(event, ui) {
+    $(this.el).addClass('note-drag');
+  },
 
-    dragStop: function(event, ui) {
-      $(this.el).removeClass('note-drag');
-      this.model.set({
-        left: ui.position.left,
-        top:  ui.position.top
-      }, {silent: true});
-    }
-  })
+  dragStop: function(event, ui) {
+    $(this.el).removeClass('note-drag');
+    this.model.set({
+      left: ui.position.left,
+      top:  ui.position.top
+    }, {silent: true});
+  }
+});
 
-
-};
 
 
 // demo time:
